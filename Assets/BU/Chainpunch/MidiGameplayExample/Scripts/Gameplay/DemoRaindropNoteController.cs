95a3403b08f1a9533a9b467ff7bf1634154425bc
OATT-PC <titipan.meta@bumail.net>
2023-11-07 23:38:40 +0700

    Add Emotion Mascot in FeedbackUI and Fixbug Mascot Spawn

--- Assets/BU/Chainpunch/MidiGameplayExample/Scripts/Gameplay/DemoRaindropNoteController.cs
+++
@@ -1,9 +0,0 @@
-using Notero.MidiGameplay.Core;
-using Notero.Raindrop;
-
-namespace BU.Chainpunch.MidiGameplay
-{
-    public class DemoRaindropNoteController : BaseRaindropNoteController, IMusicNotationControllable
-    {
-    }
-}
